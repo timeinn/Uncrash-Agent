072d675967f1a82269139ef424ae1026fc22ce2a
Sendya <18x@loacg.com>
2021-01-13 20:02:07 +0800

    fix

--- uncrash-agent.sh
+++ uncrash-agent.sh
@@ -13,7 +13,7 @@
 PATH=/sbin:/bin:/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin
 
 version="1.0.0.0013"
-uncrash_srv="http://172.20.2.190:18505/agent/logs"
+uncrash_srv="http://172.20.2.190:8080/api/agent"
 
 # Authentication required
 if [ -f /var/uncrash/auth.token ]
@@ -242,7 +242,7 @@ ping_us=$(prep $(num "$(ping -c 2 -w 1 ping-us.node.uncrash.net | grep rtt | cut
 stopTime "Get network latency"
 
 # Build data for post
-data_post="token=${auth[0]}&data=$(base "$version") $(base "$uptime") $(base "$sessions") $(base "$processes") $(base "$processes_array") $(base "$file_handles") $(base "$file_handles_limit") $(base "$os_kernel") $(base "$os_name") $(base "$os_arch") $(base "$cpu_name") $(base "$cpu_cores") $(base "$cpu_freq") $(base "$ram_total") $(base "$ram_usage") $(base "$swap_total") $(base "$swap_usage") $(base "$disk_array") $(base "$disk_total") $(base "$disk_usage") $(base "$connections") $(base "$nic") $(base "$ipv4") $(base "$ipv6") $(base "$rx") $(base "$tx") $(base "$rx_gap") $(base "$tx_gap") $(base "$load") $(base "$load_cpu") $(base "$load_io") $(base "1:$ping_cn,2:$ping_hk,3:$ping_jp,4:$ping_sg,5:$ping_eu,6:$ping_us")"
+data_post="token=${auth[0]}&data=$(base "$version") $(base "$uptime") $(base "$sessions") $(base "$processes") $(base "$processes_array") $(base "$file_handles") $(base "$file_handles_limit") $(base "$os_kernel") $(base "$os_name") $(base "$os_arch") $(base "$cpu_name") $(base "$cpu_cores") $(base "$cpu_freq") $(base "$ram_total") $(base "$ram_usage") $(base "$swap_total") $(base "$swap_usage") $(base "$disk_array") $(base "$disk_total") $(base "$disk_usage") $(base "$connections") $(base "$nic") $(base "$ipv4") $(base "$ipv6") $(base "$rx") $(base "$tx") $(base "$rx_gap") $(base "$tx_gap") $(base "$load") $(base "$load_cpu") $(base "$load_io") $(base "1:$ping_cn;2:$ping_hk;3:$ping_jp;4:$ping_sg;5:$ping_eu;6:$ping_us;")"
 
 # API request with automatic termination
 if [ -n "$(command -v timeout)" ]
